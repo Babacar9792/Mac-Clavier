41987941d33119599f227fc9e343171dbc3b4a4b
babacar <babacar77979204@gmail.com>
2023-12-29 10:14:23 +0000

    add key press for keybord

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const contenue = document.querySelectorAll(".contenue")
-const ecran = document.querySelector(".ecran")
+const contenue = document.querySelectorAll(".contenue");
+const ecran = document.querySelector(".ecran");
 // console.log(contenue)
+const key_p = document.querySelector("#key_p");
+document.addEventListener('keypress', (e) => {
+    console.log(e);
+    if(e.key==':'){
+        console.log("ok");
+    }
+    beussiClavierbi(e.key);
+})
 
 contenue.forEach(touc => {
-    touc.addEventListener("click", ()=> {
+    touc.addEventListener("click", () => {
         let val = ""
         console.log(touc.innerText)
         val = val + touc.innerText
         let spane = document.createElement("p");
-   
-        spane.innerHTML =`  <span>${val}</span>`
-            ecran.appendChild(spane)
+        spane.innerHTML = `  <span>${val}</span>`
+        ecran.appendChild(spane)
     });
-    
+
 });
+
+function beussiClavierbi(key) {
+
+    const button = document.querySelector('#key_' + key);
+    ecran.textContent += key;
+    if (button) {
+        button.classList.add('takk');
+        setTimeout(() => {
+            button.classList.remove('takk');
+        }, 200);
+    }
+}
